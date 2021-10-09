f0faa1e38f8f89dfa83c66592d99bd76b774bd94
Maksim Reznikov <Maksim_Reznikov@epam.com>
2021-10-09 21:37:22 +0300

    Add CreateOrder Azure function

---
+++ src/OrderItemsReserver/CreateOrder.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace OrderItemsReserver
+{
+    public static class CreateOrder
+    {
+        [FunctionName("create_order")]
+        public static async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, 
+            IBinder binder,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            
+            var orderId = Guid.NewGuid();
+            var outboundBlob = new BlobAttribute($"orders/{orderId}.json", FileAccess.Write);
+            await using var writer = binder.Bind<Stream>(outboundBlob);
+
+            var serializedData = JsonConvert.SerializeObject(data);
+            var encoder = new UnicodeEncoding();
+            await writer.WriteAsync(encoder.GetBytes(serializedData));
+
+            return new OkObjectResult(orderId);
+        }
+    }
+}
